ced03e4d26bb325336a434ba654fa3653401b950
User <schober2@msu.edu>
2015-04-06 08:46:58 -0400

    changed regex to numericality of

--- app/models/realestate.rb
+++ app/models/realestate.rb
@@ -1,9 +1,9 @@
 class Realestate < ActiveRecord::Base
   validates_presence_of :address,:sqfootage,:price,:bed,:bath,:constructed
   validates_format_of :address, with: /[A-Za-z0-9\s\-]/, message: "can only contain letters, numbers, spaces and dashes"
-  validates_format_of :sqfootage, with: /[1-9][0-9][0-9][0-9]?[0-9]?/, message: "isn't a sqfootage"
-  validates_format_of :price, with: /[1-9][0-9][0-9][0-9][0-9]?[0-9]?[0-9]?[0-9]?/, message: "must be between 1000 and 99999999"
-  validates_format_of :bed, with: /[1-9][0-9]?/, message: "must be between 1 and 99 bedrooms"
-  validates_format_of :bath, with: /[1-9][0-9]?/, message: "must be between 1 and 99 bathrooms"
-  validates_format_of :constructed, with: /[1][8-9][0-9][0-9]|[2][0][0][0-9]|[2][0][1][0-5]/, message: "must be betweeen 1800 and 2015"
+  validates_numericality_of :sqfootage, on: :create, :greater_than => 99, :less_than => 10001, message: "must be between 100 and 10000"
+  validates_numericality_of :price, on: :create, :greater_than => 999, :less_than => 100000000, message: "must be between 1000 and 99999999"
+  validates_numericality_of :bed, on: :create, :greater_than => 0, :less_than => 100, message: "must be between 1 and 99"
+  validates_numericality_of :bath, on: :create, :greater_than => 0, :less_than => 100, message: "must be between 1 and 99"
+  validates_numericality_of :constructed, on: :create, :greater_than => 1799, :less_than => 2016, message: "must be between 1800 and 2015"
 end
